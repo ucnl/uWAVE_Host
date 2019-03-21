afbafb7d6fdb5df78cf96a9af76e8f4bffa9d664
Aleksandr <aleksandr.v.dikarev@gmail.com>
2019-03-22 00:53:01 +0400

    Add files via upload

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("uWAVE_Host")]
-[assembly: AssemblyDescription("uWAVE underwater acoustic modem host application")]
+[assembly: AssemblyDescription("uWAVE underwater acoustic modem utility")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Underwater communication & navigation laboratory")]
+[assembly: AssemblyCompany("Underwater communication & navigation laboratory, LLC")]
 [assembly: AssemblyProduct("uWAVE_Host")]
-[assembly: AssemblyCopyright("Copyright © Aleksandr Dikarev 2017-2018")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Aleksandr Dikarev, 2018-2019")]
+[assembly: AssemblyTrademark("UC&NL")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("df012bbd-9ee4-42ec-b3da-f127d79899cb")]
+[assembly: Guid("aeebbf54-5046-4f3b-abd8-54f7c0727adf")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
